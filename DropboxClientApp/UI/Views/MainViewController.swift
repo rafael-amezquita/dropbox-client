511bfaebb76fa69cf384db984df153259a321aaf
Rafael Amezquita <rafael.amezquita@Rafaels-MacBook-Pro.local>
2020-05-25 11:20:27 -0500

    -restoring authentication -

--- DropboxClientApp/UI/Views/MainViewController.swift
+++ DropboxClientApp/UI/Views/MainViewController.swift
@@ -25,12 +25,12 @@ class MainViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        //if isUserAuthenticated() {
+        if isUserAuthenticated() {
             let navigation = NavigationViewController()
             present(navigation, animated: true, completion: nil)
-        //} else {
-          //  callAuthorizationController()
-        //}
+        } else {
+            callAuthorizationController()
+        }
     }
     
     // MARK: - Configuration
